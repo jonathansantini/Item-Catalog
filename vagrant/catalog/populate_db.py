e3db2bce4359db6b233abd4c04bfcf8cb9f5d41f
Jonathan Santini <jsantini@jonathansantini.com>
2019-07-28 10:58:18 -0400

    feat: Added database setup and population script.
    The database setup and population files allow a user to get up
    and going with the database used by this flask application.

---
+++ vagrant/catalog/populate_db.py
@@ -0,0 +1,58 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Category, Base, CategoryItem
+
+engine = create_engine('sqlite:///categories.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+
+# First Category
+category1 = Category(name="Albums")
+
+session.add(category1)
+session.commit()
+
+categoryItem1 = CategoryItem(name="Ok Computer", description="OK Computer is the third studio album by English rock band Radiohead, released on 16 June 1997 on EMI subsidiaries Parlophone and Capitol Records. The members of Radiohead self-produced the album with Nigel Godrich, an arrangement they have used for their subsequent albums. Other than the song 'Lucky', recorded in 1995, Radiohead recorded OK Computer in Oxfordshire and Bath between 1996 and early 1997, mostly in the historic mansion St Catherine's Court. The band distanced themselves from the guitar-centred, lyrically introspective style of their previous album, The Bends. OK Computer's abstract lyrics, densely layered sound and eclectic influences laid the groundwork for Radiohead's later, more experimental work.",
+                     category=category1)
+
+session.add(categoryItem1)
+session.commit()
+
+# Second Category
+category2 = Category(name="Books")
+
+session.add(category2)
+session.commit()
+
+categoryItem1 = CategoryItem(name="One Hundred Years of Solitude", description="One Hundred Years of Solitude is a landmark 1967 novel by Colombian author Gabriel Garcia Marquez that tells the multi-generational story of the Buendia family, whose patriarch, Jose Arcadio Buendia, founded the town of Macondo, a fictitious town in the country of Colombia.",
+                     category=category2)
+
+session.add(categoryItem1)
+session.commit()
+
+# Third Category
+category3 = Category(name="Video Games")
+
+session.add(category3)
+session.commit()
+
+categoryItem1 = CategoryItem(name="Metal Gear Solid", description="Metal Gear Solid[a] is an action-adventure stealth video game developed by Konami Computer Entertainment Japan and released for the PlayStation in 1998. The game was directed, produced, and written by Hideo Kojima, and serves as a sequel to the MSX2 video games Metal Gear and Metal Gear 2: Solid Snake, which Kojima also worked on. The game was first unveiled in the Tokyo Game Show in 1996 and subsequently shown in the Electronic Entertainment Expo in 1997, before eventually releasing in late 1998.",
+                     category=category3)
+
+session.add(categoryItem1)
+session.commit()
+
+print "added category items!"
