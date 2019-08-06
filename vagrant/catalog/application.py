a2720240c7360dd59656618e41c8c1f574c76a89
Jonathan Santini <jsantini@jonathansantini.com>
2019-08-05 23:36:39 -0400

    feat: Added google plus login logic, templates and functionality.

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -12,6 +12,17 @@ app.config.from_pyfile('instance/application.cfg')
 from flask import session as login_session
 import random, string
 
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "Catalog App"
+
 engine = create_engine('sqlite:///categories.db', connect_args={'check_same_thread': False})
 Base.metadata.bind = engine
 
@@ -26,6 +37,119 @@ def showLogin():
     login_session['state'] = state
     return "The current session state is %s" % login_session['state']
 
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Obtain authorization code
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
+
+@app.route('/gdisconnect')
+def gdisconnect():
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        print 'Access Token is None'
+        response = make_response(json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    print 'In gdisconnect access token is %s', access_token
+    print 'User name is: '
+    print login_session['username']
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    print 'result is '
+    print result
+    if result['status'] == '200':
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
 @app.route('/')
 @app.route('/categories/')
 def showCategories():
@@ -35,6 +159,8 @@ def showCategories():
 
 @app.route('/category/new/', methods=['GET', 'POST'])
 def addCategory():
+  if 'username' not in login_session:
+    return redirect('/login')
   if request.method == 'POST':
     addCategory = Category(name=request.form['name'])
     session.add(addCategory)
@@ -45,6 +171,8 @@ def addCategory():
 
 @app.route('/category/<int:category_id>/edit/', methods=['GET', 'POST'])
 def editCategory(category_id):
+  if 'username' not in login_session:
+    return redirect('/login')
   editedCategory = session.query(Category).filter_by(id = category_id).one()
   if request.method == 'POST':
     if request.form['name']:
@@ -57,6 +185,8 @@ def editCategory(category_id):
 
 @app.route('/category/<int:category_id>/delete/', methods=['GET', 'POST'])
 def deleteCategory(category_id):
+  if 'username' not in login_session:
+    return redirect('/login')
   itemToDelete = session.query(Category).filter_by(id = category_id).one()
   if request.method == 'POST':
     session.delete(itemToDelete)
@@ -85,6 +215,8 @@ def showItem(category_id, item_id):
 
 @app.route('/category/<int:category_id>/item/new/', methods=['GET', 'POST'])
 def addItem(category_id):
+  if 'username' not in login_session:
+    return redirect('/login')
   if request.method == 'POST':
     newCategoryItem = CategoryItem(name=request.form['name'], description=request.form['desc'], category_id=category_id)
     session.add(newCategoryItem)
@@ -95,6 +227,8 @@ def addItem(category_id):
 
 @app.route('/category/<int:category_id>/item/<int:item_id>/edit/', methods=['GET', 'POST'])
 def editItem(category_id, item_id):
+  if 'username' not in login_session:
+    return redirect('/login')
   itemToEdit = session.query(CategoryItem).filter_by(id = item_id).one()
   if request.method == 'POST':
     if request.form['name']:
@@ -109,6 +243,8 @@ def editItem(category_id, item_id):
 
 @app.route('/category/<int:category_id>/item/<int:item_id>/delete/', methods=['GET', 'POST'])
 def deleteItem(category_id, item_id):
+  if 'username' not in login_session:
+    return redirect('/login')
   itemToDelete = session.query(CategoryItem).filter_by(id = item_id).one()
   if request.method == 'POST':
     session.delete(itemToDelete)
